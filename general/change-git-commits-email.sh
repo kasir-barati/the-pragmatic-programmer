f87b2c201bc0362f12e71ae236083f518a67edcb
Mohammad Jawad (Kasir) Barati <kasir.barati@gmail.com>
2026-07-31 20:52:06 +0200

    chore: enhance change-git-commits-email.sh with error handling

--- general/change-git-commits-email.sh
+++ general/change-git-commits-email.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+if ! git filter-repo --version >/dev/null 2>&1; then
+    echo "Error: git-filter-repo is not installed or not available in PATH. You can install it on"
+    echo "Ubuntu: sudo apt update && sudo apt install git-filter-repo"
+    exit 1
+fi
+
 usage() {
     cat <<EOF
 Usage:
@@ -35,7 +41,7 @@ fi
 NUM_COMMITS="$1"
 NEW_EMAIL="$2"
 
-git filter-repo \
+git filter-repo --force \
     --commit-callback "
 if commit.original_id in [c.original_id for c in repo.get_commits('HEAD~${NUM_COMMITS}..HEAD')]:
     commit.author_email = b'$NEW_EMAIL'
